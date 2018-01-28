6c95759c7707055b3532401ee8228f095ca5e8a8
Megharth <m.c.lakhatarai@gmial.com>
2018-01-28 21:22:28 +0530

    added-gallery

--- src/components/gallery.vue
+++ src/components/gallery.vue
@@ -2,7 +2,9 @@
   <div>
     <app-header></app-header>
     <div class="container">
-
+      <b-card v-for="img in images">
+        <img :src="img.imgURL" class="imgIco d-block mx-auto">
+      </b-card>
     </div>
   </div>
 </template>
@@ -16,10 +18,33 @@ export default {
   components: {
     'app-header': Header
   },
-  mounted() {
+  data() {
+    return {
+      images: null
+    }
+  },
+  mounted: function () {
     database.ref('images/').once('value').then(snapshot => {
-      console.log(snapshot.val());
+      this.images = snapshot.val();
     })
   }
 }
 </script>
+<style scoped>
+.card {
+  width: 200px;
+  display: inline-block;
+  padding: 0;
+  border: hidden;
+}
+.card-body{
+  padding: 0;
+  width: 200px;
+}
+.imgIco{
+  height: 200px;
+  max-width: 250px;
+  padding: 0;
+  margin: 0;
+}
+</style>
